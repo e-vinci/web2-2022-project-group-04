324034c88dca796bf8dd8b531163aec040fd7df2
7Matteo <matteo.fiore@student.vinci.be>
2022-11-24 15:47:24 +0100

    update : route to /login in routes.js

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,8 +1,10 @@
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import LoginPage from '../Pages/LoginPage';
 
 const routes = {
   '/': HomePage,
+  '/login': LoginPage,
   '/new': NewPage,
 };
 
