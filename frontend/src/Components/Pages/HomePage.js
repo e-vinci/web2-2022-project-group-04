38cce23543170ea3a02c0b164677e897ca0874ed
7Matteo <matteo.fiore@student.vinci.be>
2022-11-28 09:39:47 +0100

    update: frontend HomePage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -2,9 +2,18 @@ import { clearPage } from "../../utils/render";
 
 const renderHomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = `<div class="container">
-                      <div class="text-center">
-                        <button type="button" class="btn btn-primary btn-lgg">S'inscrire</button>
+  main.innerHTML = `<div class="homePageDiv">
+                      <div class="container">
+                        <div class="text-center">
+                          <h1 class="sloganText1">Facilitez vous la vie,<br> 
+                          trouvez un job avec DevJob
+                          </h1>
+                          <h3 class="sloganText2">Créé par des devs pour des devs
+                          </h3>
+                          <a href="" data-uri="/RegisterPage>
+                            <button id="signUpButton" type="button" class="btn btn-primary btn-lgg">S'inscrire</button>
+                          </a>
+                        </div>
                       </div>
                     </div>`;
 };
