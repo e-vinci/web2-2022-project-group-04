b5ae876d816b8052c9c6fb6b97a5a0b90678e009
Oussama <oussama.elbouhtani@student.vinci.com>
2022-11-28 10:11:54 +0100

    feat : update HomePage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,13 +1,24 @@
 import { clearPage } from "../../utils/render";
+import Navigate from "../Router/Navigate";
 
 const renderHomePage = () => {
   const main = document.querySelector('main');
   main.innerHTML = `<div class="container">
                       <div class="text-center">
-                        <button type="button" class="btn btn-primary btn-lgg">S'inscrire</button>
+                        <button id="btn-register" type="button" class="btn btn-primary btn-lgg">S'inscrire</button>
                       </div>
                     </div>`;
 };
+
+
+const registerButton = document.getElementById('btn-register');
+registerButton.addEventListener(onclick, (e) => {
+  e.preventDefault();
+  const uri = e.target?.dataset?.uri;
+  Navigate(uri);
+}
+);
+
 
 
 const Homepage = () => {
