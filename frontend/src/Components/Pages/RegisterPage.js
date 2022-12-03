a581280085e7725a83699f27cef921bfb2b22f2a
Oussama <oussama.elbouhtani@student.vinci.com>
2022-12-03 11:50:44 +0100

    Feat : clean up HomePage, RegisterPage

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -1,24 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { clearPage } from "../../utils/render";
+import { clearPage } from '../../utils/render';
 
 const main = document.querySelector('main');
+main.style = 'background-color : azure;';
 
 const renderRegisterFormDevPage = () => {
-    main.innerHTML = `<form id ="registerFormDevelopper">
+  main.innerHTML = `<form id ="registerFormDevelopper">
                             <div class="form-group">
                                 <label for="Nom">Nom</label>
-                                <input type="text" class="form-control" id="idNom" aria-describedby="lastNameHelp" placeholder="Entrez votre nom">
+                                <input type="text" class="form-control" id="idLastname" aria-describedby="lastNameHelp" placeholder="Entrez votre nom">
                             </div>
 
                             <div class="form-group">
                                 <label for="Prenom">Prénom</label>
-                                <input type="text" class="form-control" id="idPrenom" aria-describedby="firstNameHelp" placeholder="Entrez votre prénom">
+                                <input type="text" class="form-control" id="idFirstname" aria-describedby="firstNameHelp" placeholder="Entrez votre prénom">
                             </div>
 
                             <div class="form-group">
                                 <label for="email">E-mail</label>
                                 <input type="email" class="form-control" id="idEmail" placeholder="ex: johndoe@outlook.com">
-                                <small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais votre e-mail avec quelqu'un d'autre.</small>
                             </div>
 
                             <div class="form-group">
@@ -30,17 +30,12 @@ const renderRegisterFormDevPage = () => {
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                 <label class="form-check-label" for="exampleCheck1">Se souvenir de moi</label>
                             </div>
-                            <button type="submit" class="btn btn-primary">S'enregistrer</button>
+                            <button id="register" type="submit" class="btn btn-primary">S'enregistrer</button>
                             </form>`;
 };
 const RegisterFormDevPage = () => {
-    clearPage();
-    renderRegisterFormDevPage();
-  };
-
-  
+  clearPage();
+  renderRegisterFormDevPage();
+};
 
 export default RegisterFormDevPage;
-
-  
-                            
