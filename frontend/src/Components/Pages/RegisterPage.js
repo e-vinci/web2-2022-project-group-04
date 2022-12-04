eff0d338de59e229af616fd46881d88af74a1dc8
Youssef <youssef.abouhamid@student.vinci.be>
2022-12-04 17:39:28 +0100

    Front : problème avec les écouteur d'évenement

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -2,13 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { clearPage } from '../../utils/render';
 
 const main = document.querySelector('main');
+
 const renderRegisterFormDevPage = () => {
-        main.innerHTML = `<div class="contener-fluid 
+  clearPage();
+  // eslint-disable-next-line no-console
+  console.log('affiche Form Dev Page');
+  main.innerHTML = `<div class="contener-fluid 
         bg-warning row justify-content-evenly">
-        <div class="col-6 text-center bg-secondary text-white">
+        <div class="col-6 text-center bg-secondary text-white " id="clickForFormDev">
        Développeur
       </div>
-      <div class="col-6 text-center bg-light">
+      <div class="col-6 text-center bg-light" id="clickForFormCompanie">
       Entreprise
       </div>
         </div>
@@ -61,7 +65,7 @@ const renderRegisterFormDevPage = () => {
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1" />
           <label class="form-check-label" for="exampleCheck1"
-            >en soumettant ce formulaire, j'accepte que DevJob utilise mes coordonnées dans le strict cadre de nos service tout en respectant le <a
+            >en soumettant ce formulaire, j'accepte que DevJob utilise mes données dans le strict cadre de nos service tout en respectant le <a
             href="https://eur-lex.europa.eu/legal-content/FR/TXT/HTML/?uri=CELEX:32016R0679&from=FR"
             >RGPD</a.</label
           >
@@ -69,9 +73,87 @@ const renderRegisterFormDevPage = () => {
         <button type="submit" class="btn btn-primary">S'enregistrer</button>
       </form>`;
 };
-const RegisterFormDevPage = () => {
+// eslint-disable-next-line no-unused-vars
+const renderRegisterFormCompaniesPage = () => {
   clearPage();
-  renderRegisterFormDevPage();
+  // eslint-disable-next-line no-console
+  console.log('affiche Form Companies Page');
+
+  main.innerHTML = `<div class="contener-fluid 
+  bg-warning row justify-content-evenly">
+  <div class="col-6 text-center bg-light   " id="clickForFormDev">
+ Développeur
+</div>
+<div class="col-6 text-center bg-secondary text-white" id="clickForFormCompanie">
+Entreprise
+</div>
+  </div>
+  <form id="registerFormDevelopper" class="white p-3 container">
+  <div class="form-group">
+    <label for="Nom">Nom</label>
+    <input
+      type="text"
+      class="form-control"
+      id="idNom"
+      aria-describedby="lastNameHelp"
+      placeholder="Entrez votre nom"
+    />
+  </div>
+
+  <div class="form-group">
+    <label for="Prenom">Prénom</label>
+    <input
+      type="text"
+      class="form-control"
+      id="idPrenom"
+      aria-describedby="firstNameHelp"
+      placeholder="Entrez votre prénom"
+    />
+  </div>
+
+  <div class="form-group">
+    <label for="email">E-mail</label>
+    <input
+      type="email"
+      class="form-control"
+      id="idEmail"
+      placeholder="ex: johndoe@outlook.com"
+    />
+    <small id="emailHelp" class="form-text text-muted"
+      >Nous ne partagerons jamais votre e-mail avec quelqu'un d'autre.</small
+    >
+  </div>
+
+  <div class="form-group">
+    <label for="Password">Mot de passe</label>
+    <input
+      type="password"
+      class="form-control"
+      id="idPassword"
+      placeholder="(min. 8 caractères, 1 majuscule et 1 chiffre)"
+    />
+  </div>
+
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1"
+      >en soumettant ce formulaire, j'accepte que DevJob utilise mes données dans le strict cadre de nos service tout en respectant le <a
+      href="https://eur-lex.europa.eu/legal-content/FR/TXT/HTML/?uri=CELEX:32016R0679&from=FR"
+      >RGPD</a.</label
+    >
+  </div>
+  <button type="submit" class="btn btn-primary">S'enregistrer</button>
+</form>`;
 };
 
-export default RegisterFormDevPage;
+const RegisterFormPage = () => {
+  const buttonDisplayFormDev = document.querySelector('#clickForFormDev');
+  const buttonDisplayFormCompanie = document.querySelector('#clickForFormCompanie');
+  renderRegisterFormCompaniesPage();
+  // eslint-disable-next-line spaced-comment
+  //buttonDisplayFormDev.addEventListener('click', ()=>{renderRegisterFormDevPage()});
+  buttonDisplayFormDev.addEventListener('click', ()=>{console.log("");});
+  buttonDisplayFormCompanie.addEventListener('click', ()=>{renderRegisterFormCompaniesPage()});
+};
+
+export default RegisterFormPage;
