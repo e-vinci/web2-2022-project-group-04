76b87011785053119bfe5b39e161b8c762817174
Oussama <oussama.elbouhtani@student.vinci.com>
2022-12-05 20:42:30 +0100

    feat : registerPage - onRegister Event

--- api/app.js
+++ api/app.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieSession = require('cookie-session');
 const client = require('./connection');
+
 const developersRouter = require('./routes/developers');
 const pizzaRouter = require('./routes/pizzas');
 const jobOffersRouter = require('./routes/jobOffers');
@@ -14,6 +16,20 @@ client.connect((err) => {
   if (err) console.error("Error in connecting to the database",err);
   else console.log("Connection to the database ...");
 });
+
+const expiryDateIn3Months = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 3);
+const cookieSecreteKey = 'YouWouldnot!not!like!mypizza';
+app.use(
+  cookieSession({
+    name: 'user',
+    keys: [cookieSecreteKey],
+    cookie: {
+      httpOnly: true,
+      expires: expiryDateIn3Months,
+    },
+  }),
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
