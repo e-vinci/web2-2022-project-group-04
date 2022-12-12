369ab4bb48ce295a433fa01ac2fe9f6fe64ab1bc
Youssef <youssef.abouhamid@student.vinci.be>
2022-12-12 11:49:59 +0100

    front : resolution de conflit registerPage

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -41,7 +41,6 @@ Entreprise
    />
  </div>
 
-<<<<<<< HEAD
  <div class="form-group">
    <label for="email">E-mail</label>
    <input
@@ -97,79 +96,6 @@ Entreprise
 </form>`;
 
 }
-=======
-  <div class="form-group">
-    <label for="Nom">Nom</label>
-    <input
-      type="text"
-      class="form-control"
-      id="idNom"
-      aria-describedby="lastNameHelp"
-      placeholder="Entrez votre nom"
-    />
-  </div>
-
-  <div class="form-group">
-    <label for="Prenom">Prénom</label>
-    <input
-      type="text"
-      class="form-control"
-      id="idPrenom"
-      aria-describedby="firstNameHelp"
-      placeholder="Entrez votre prénom"
-    />
-  </div>
-
-  <div class="form-group">
-    <label for="email">E-mail</label>
-    <input
-      type="email"
-      class="form-control"
-      id="idEmail"
-      placeholder="ex: johndoe@outlook.com"
-    />
-   
-  </div>
-
-  <div class="form-group">
-    <label for="tel">numéro de téléphone</label>
-    <input
-      type="tel"
-      class="form-control"
-      id="idPhoneNumber"
-      placeholder="ex: 0412 34 56 90"
-    />
-   
-  </div>
-
-  <div class="form-group">
-    <label for="Password">Mot de passe</label>
-    <input
-      type="password"
-      class="form-control"
-      id="idPassword"
-      placeholder="(min. 8 caractères, 1 majuscule et 1 chiffre)"
-    />
-  </div>
-
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1"
-      >en soumettant ce formulaire, j'accepte que DevJob utilise mes données dans le strict cadre de nos service tout en respectant le <a
-      href="https://eur-lex.europa.eu/legal-content/FR/TXT/HTML/?uri=CELEX:32016R0679&from=FR"
-      >RGPD</a.</label
-    >
-  </div>
-  <button type="submit" class="btn btn-primary">S'enregistrer</button>
-</form>`;
-}
-const renderRegisterFormDevPage = () => {
-
-  clearPage()
-  main.innerHTML += renderRegisterFormDevPageAsString();
-  attachListeners();
-};
->>>>>>> 61fba41863bea7c6bec009c6bc55398ab614bfe0
 function renderRegisterFormCompaniesPageAsString() {
   return `<div class="container-fluid 
    row justify-content-evenly">
@@ -318,7 +244,3 @@ function attachListeners() {
 }
 
 export default RegisterFormPage;
-<<<<<<< HEAD
-=======
-
->>>>>>> 61fba41863bea7c6bec009c6bc55398ab614bfe0
