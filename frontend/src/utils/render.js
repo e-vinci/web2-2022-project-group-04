2c33540696febbab03244e44c0cbcc1eb98c92b4
7Matteo <matteo.fiore@student.vinci.be>
2022-12-17 12:29:39 +0100

    update : render page title

--- frontend/src/utils/render.js
+++ frontend/src/utils/render.js
@@ -6,9 +6,10 @@ const clearPage = () => {
 const renderPageTitle = (title) => {
   if (!title) return;
   const main = document.querySelector('main');
-  const pageTitle = document.createElement('h4');
-  pageTitle.innerText = title;
-  main.appendChild(pageTitle);
+  main.innerHTML = `
+  <div class="container py-3 w-25 mx-auto rounded-2 text-center t">
+    <h2 id="titlePages" class="">${title}</h2>
+  </div>`
 };
 
 export { clearPage, renderPageTitle };
